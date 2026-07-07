f6ceee3e5514c05f6da706e24af55c1e609bcc18
Krishna27-IT <patil2006.krishna@gmail.com>
2026-07-07 23:33:05 +0530

    Solve #8. String To Integer (atoi)

---
+++ String/8_String_To_Integer.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+#include<climits>
+using namespace std;
+
+int main(){
+    string s ="-21474836479";
+    int i=0,result=0,sign=1,digit=0;
+
+    while(s[i] == ' '){
+        i++;
+    }
+
+    if(s[i] == '+' || s[i] == '-'){
+        if(s[i] == '-'){
+            sign = -1;
+        }
+        i++;
+    }
+
+    while(s[i] >= '0' && s[i] <= '9'){
+        digit = s[i] - '0';
+        int limit = (sign == 1) ? 7 : 8;
+        if(sign == 1){
+            if(result > INT_MAX/10 || (result == INT_MAX/10 && digit > limit)){
+                cout<<"String To Integer: "<<INT_MAX;
+                return INT_MAX;
+            }
+            result = result * 10 + digit;
+        }else{
+            if(result < INT_MIN/10 || (result == INT_MIN/10 && digit > limit)){
+                cout<<"String To Integer: "<<INT_MIN;
+                return INT_MIN;
+            }
+            result = result * 10 - digit;
+        }
+        i++;
+    }
+
+    cout<<"String To Integer: "<<result*sign;
+    return 0;
+}
